48ff89afa98cace29aadac96a97e2b26d7d27a2a
Ermiyas  Hailemichael <yemeerma11@gmail.com>
2022-10-16 12:22:09 -0700

    server.js: Add a new get route for /drinks/:id

--- server.js
+++ server.js
@@ -4,17 +4,23 @@ const app = express();
 const drinks = require('./models/drinks')
 
 
+//Check that you have your Welcome to the Gitpub App!message displaying
+app.get('/',(req, res)=>{
+    res.send('Welcome to the Gitpub App!')
+})
+
+// Create a get route /drinks that will res.send(drinks),which will display your drinks data as json in the browser
+// Change your /drinks route to res.renderyour drinks_index.ejs
 app.get('/drinks', (req, res)=>{
     // res.send(drinks)
     res.render('drinks_index.ejs',{alldrinks: drinks})
 })
 
-app.get('/',(req, res)=>{
-    res.send('Welcome to the Gitpub App!')
-})
-
-
-
+// Setting up your show route
+// Add a new get route for /drinks/:id
+ app.get('drinks/:id',(req, res)=>{
+    res.send(req.params.id)
+ })
 
 
 
