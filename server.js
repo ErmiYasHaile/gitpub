33159b5f3731aa9d241b3dac02c20e20fe5a537a
Ermiyas  Hailemichael <yemeerma11@gmail.com>
2022-10-16 13:56:02 -0700

    server.js: Update the get route to render the showview with the drinks data

--- server.js
+++ server.js
@@ -1,31 +1,37 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
-const drinks = require('./models/drinks')
-
+const drinks = require("./models/drinks");
 
 //Check that you have your Welcome to the Gitpub App!message displaying
-app.get('/',(req, res)=>{
-    res.send('Welcome to the Gitpub App!')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to the Gitpub App!</h1>");
+  
+});
 
 // Create a get route /drinks that will res.send(drinks),which will display your drinks data as json in the browser
 // Change your /drinks route to res.renderyour drinks_index.ejs
-app.get('/drinks', (req, res)=>{
-    // res.send(drinks)
-    res.render('drinks_index.ejs',{alldrinks: drinks})
-})
+app.get("/drinks", (req, res) => {
+//   res.send(drinks)
+  res.render("drinks_index.ejs", {
+    alldrinks: drinks,
+  });
+});
 
 // Setting up your show route
 // Add a new get route for /drinks/:id
- app.get('drinks/:id',(req, res)=>{
-    res.send(req.params.id)
- })
+app.get("/drinks/:id", (req, res) => {
+//   res.send(req.params.id);
+  res.render("drinks_show.ejs", {
+    alldrinks: drinks[req.params.id],
+  });
+});
 
 
 
 
 
-app.listen(3000,()=>{
-    console.log('listening to port 3000...')
-})
+
+app.listen(3000, () => {
+  console.log("listening to port 3000...");
+});
