5b4e7cf1376fc4b52c5a0a18d30204021c979181
Ermiyas  Hailemichael <yemeerma11@gmail.com>
2022-10-16 14:51:53 -0700

    food_index.ejs and food_show.ejs connected

--- server.js
+++ server.js
@@ -2,17 +2,23 @@ const express = require("express");
 const app = express();
 
 const drinks = require("./models/drinks");
+const food = require("./models/food");
 
 //Check that you have your Welcome to the Gitpub App!message displaying
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to the Gitpub App!</h1>");
-  
+});
+
+// foods list
+app.get("/food", (req, res) => {
+  //   res.send(food);
+  res.render("food_index.ejs", { food });
 });
 
 // Create a get route /drinks that will res.send(drinks),which will display your drinks data as json in the browser
 // Change your /drinks route to res.renderyour drinks_index.ejs
 app.get("/drinks", (req, res) => {
-//   res.send(drinks)
+  //   res.send(drinks)
   res.render("drinks_index.ejs", {
     alldrinks: drinks,
   });
@@ -21,16 +27,16 @@ app.get("/drinks", (req, res) => {
 // Setting up your show route
 // Add a new get route for /drinks/:id
 app.get("/drinks/:id", (req, res) => {
-//   res.send(req.params.id);
+  //   res.send(req.params.id);
   res.render("drinks_show.ejs", {
     alldrinks: drinks[req.params.id],
   });
 });
 
-
-
-
-
+app.get("/food/:id", (req, res) => {
+  res.render("food_show.ejs", { 
+    foodList: food[req.params.id] });
+});
 
 app.listen(3000, () => {
   console.log("listening to port 3000...");
